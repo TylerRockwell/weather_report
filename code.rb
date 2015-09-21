c5bbbee1e5831972dba8a7056edcf84e3ecc118a
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-09-21 14:58:14 -0400

    Add 10 day forecast

--- code.rb
+++ code.rb
@@ -1,5 +1,6 @@
 require './current_conditions'
+require './forecast'
 
-puts "Please enter a zip code: "
-
-current_conditions = CurrentConditions(gets.chomp)
+# puts "Please enter a zip code: "
+#
+# current_conditions = CurrentConditions(gets.chomp)
--- test.rb
+++ test.rb
@@ -1,11 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './current_conditions'
+require './code'
 require 'json'
 
 class CurrentConditions
   private def get_response
     info_file = File.read('current_conditions_durham.json')
+    JSON.parse(info_file)
+  end
+end
+
+class Forecast
+  private def get_response
+    info_file = File.read('forecast_durham.json')
     JSON.parse(info_file)
   end
 end
@@ -44,5 +51,24 @@ class WeatherReportTest < Minitest::Test
     Humidity is at: 90%.\n
     The dewpoint is: 69 F (20 C).\n
     Current wind speed is: 0.0 MPH NW", CurrentConditions.new(27701).get_it_all_pretty
+  end
+
+  def test_get_1_day_forecast_text_08
+    assert_equal "Monday:\nOvercast with rain showers at times. Thunder possible. High near 75F. Winds NE at 5 to 10 mph. Chance of rain 40%.\n",
+                 Forecast.new(27701).fc_text(0)
+  end
+
+  def test_get_10_day_forecast_text_09
+    assert_equal "Monday:\nOvercast with rain showers at times. Thunder possible. High near 75F. Winds NE at 5 to 10 mph. Chance of rain 40%.\n\
+Tuesday:\nCloudy early with partial sunshine expected late. Slight chance of a rain shower. High 78F. Winds NNE at 10 to 15 mph.\n\
+Wednesday:\nMostly sunny skies. High near 80F. Winds NNE at 10 to 15 mph.\n\
+Thursday:\nOvercast with rain showers at times. High 78F. Winds NNE at 10 to 20 mph. Chance of rain 60%.\n\
+Friday:\nRain likely. High 76F. Winds NE at 10 to 15 mph. Chance of rain 90%. Rainfall near a quarter of an inch.\n\
+Saturday:\nRain showers in the morning becoming more intermittent in the afternoon. High 74F. Winds NE at 10 to 15 mph. Chance of rain 60%.\n\
+Sunday:\nPartly cloudy skies during the morning hours will become overcast in the afternoon. High 74F. Winds NE at 10 to 15 mph.\n\
+Monday:\nCloudy with periods of rain. High 74F. Winds NE at 10 to 15 mph. Chance of rain 70%. Rainfall around a half an inch.\n\
+Tuesday:\nRain showers early with overcast skies later in the day. High 77F. Winds NE at 5 to 10 mph. Chance of rain 40%.\n\
+Wednesday:\nCloudy skies early, followed by partial clearing. High near 80F. Winds NE at 5 to 10 mph.\n",
+    Forecast.new(27701).ten_day
   end
 end
